9f6784eb3326fa5db1d36f9785d4dc6094bcaaf9
mloubout <mathias.louboutin@gmail.com>
2017-01-19 11:21:07 -0800

    merge master

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ except ImportError:
     from distutils.core import setup
 
 setup(name='devito',
-      version='2.0.1',
+      version='2.0.2',
       description="Finite Difference DSL for symbolic computation.",
       long_descritpion="""Devito is a new tool for performing
       optimised Finite Difference (FD) computation from high-level
