ad8be6c91a8baad82a5ab60a9b135def5311e9f0
mloubout <mathias.louboutin@gmail.com>
2017-08-15 09:53:25 -0700

    more bugs

--- examples/seismic/acoustic/bug-ex.py
+++ examples/seismic/acoustic/bug-ex.py
@@ -3,11 +3,12 @@ from sympy import Eq, exp
 
 from devito import DenseData, Dimension, TimeData, t, time, Operator, x, y, z
 from examples.seismic import PointSource
+from devito.logger import debug
 
 
 # NoneType bug at initialization as dimensions size are not know from u
 
-def bug_dimension(dimensions=(50, 50, 50), spacing=(15.0, 15.0, 15.0)):
+def bug_dimension(dimensions=(50, 50), spacing=(15.0, 15.0)):
 
     dt = 1.2
     nt = 100
@@ -16,9 +17,9 @@ def bug_dimension(dimensions=(50, 50, 50), spacing=(15.0, 15.0, 15.0)):
 
     p_truc = Dimension(name="ptruc", size=10)
     # Source location
-    location = np.zeros((1, 3), dtype=np.float32)
+    location = np.zeros((1, 2), dtype=np.float32)
     location[0, :-1] = [dimensions[i] * spacing[i] * .5
-                        for i in range(3 - 1)]
+                        for i in range(2 - 1)]
     location[0, -1] = 2 * spacing[-1]
 
     time_series = np.zeros((nt, 1))
@@ -45,7 +46,7 @@ def bug_dimension(dimensions=(50, 50, 50), spacing=(15.0, 15.0, 15.0)):
 
 # This one has two split time loops
 
-def bug_dimension2(dimensions=(50, 50, 50), spacing=(15.0, 15.0, 15.0)):
+def bug_dimension2(dimensions=(50, 50), spacing=(15.0, 15.0)):
     dt = 1.2
     nt = 100
     u = TimeData(name="u", shape=dimensions, dtype=np.float32, save=False,
@@ -55,9 +56,9 @@ def bug_dimension2(dimensions=(50, 50, 50), spacing=(15.0, 15.0, 15.0)):
 
 
     # Source location
-    location = np.zeros((1, 3), dtype=np.float32)
+    location = np.zeros((1, 2), dtype=np.float32)
     location[0, :-1] = [dimensions[i] * spacing[i] * .5
-                        for i in range(3-1)]
+                        for i in range(2-1)]
     location[0, -1] = 2 * spacing[-1]
 
     time_series = np.zeros((nt, 1))
@@ -67,7 +68,6 @@ def bug_dimension2(dimensions=(50, 50, 50), spacing=(15.0, 15.0, 15.0)):
     # Avoid dimension bug
     x.size=dimensions[0]
     y.size = dimensions[1]
-    z.size = dimensions[2]
     time.size = 100
 
     dense = DenseData(name="a", dimensions=(p_truc,) + u.indices[1:])
@@ -88,7 +88,7 @@ def bug_dimension2(dimensions=(50, 50, 50), spacing=(15.0, 15.0, 15.0)):
 
 
 # This one is good
-def NObug_dimension3(dimensions=(500, 500, 500), spacing=(15.0, 15.0, 15.0)):
+def NObug_dimension3(dimensions=(500, 500), spacing=(15.0, 15.0)):
     dt = 1.2
     nt = 100
     u = TimeData(name="u", shape=dimensions, dtype=np.float32, save=False,
@@ -97,9 +97,9 @@ def NObug_dimension3(dimensions=(500, 500, 500), spacing=(15.0, 15.0, 15.0)):
     p_truc = Dimension(name="ptruc", size=10)
 
     # Source location
-    location = np.zeros((1, 3), dtype=np.float32)
+    location = np.zeros((1, 2), dtype=np.float32)
     location[0, :-1] = [dimensions[i] * spacing[i] * .5
-                        for i in range(3 - 1)]
+                        for i in range(2 - 1)]
     location[0, -1] = 2 * spacing[-1]
 
     time_series = np.zeros((nt, 1))
@@ -109,7 +109,6 @@ def NObug_dimension3(dimensions=(500, 500, 500), spacing=(15.0, 15.0, 15.0)):
     # Avoid dimension bug
     x.size = dimensions[0]
     y.size = dimensions[1]
-    z.size = dimensions[2]
     time.size = 100
 
     dense = DenseData(name="a", dimensions=u.indices[1:] + (p_truc,))
@@ -137,13 +136,10 @@ def bug_dimension4(dimensions=(500, 500), spacing=(15.0, 15.0)):
 
     p_truc = Dimension(name="ptruc", size=10)
 
-    x.size = dimensions[0]
-    y.size = dimensions[1]
-
     # Source location
-    location = np.zeros((1, 3), dtype=np.float32)
+    location = np.zeros((1, 2), dtype=np.float32)
     location[0, :-1] = [dimensions[i] * spacing[i] * .5
-                        for i in range(3 - 1)]
+                        for i in range(2 - 1)]
     location[0, -1] = 2 * spacing[-1]
 
     time_series = np.zeros((nt, 1))
@@ -152,12 +148,12 @@ def bug_dimension4(dimensions=(500, 500), spacing=(15.0, 15.0)):
     src = PointSource(name='src', data=time_series, coordinates=location)
 
     dense = DenseData(name="a", dimensions=u.indices[1:] + (p_truc,))
-    truc = DenseData(name="tr", dimensions=(p_truc,))
+    truc = DenseData(name="tr", dimensions=(p_truc, time))
     truc.data[:] = 10.
 
     stencil1 = [Eq(u.forward, u.laplace)]
 
-    stencil2 = [Eq(dense,  dense + exp(time)*u)]
+    stencil2 = [Eq(dense,  dense + truc*u)]
 
     subs = dict([(t.spacing, dt)] + [(time.spacing, dt)] +
                 [(i.spacing, spacing[j]) for i, j
@@ -168,4 +164,32 @@ def bug_dimension4(dimensions=(500, 500), spacing=(15.0, 15.0)):
     op.cfunction
 
 if __name__ == "__main__":
-    bug_dimension4()
+    print("First test")
+    try:
+        bug_dimension()
+    except:
+        print("Bug 1 supposed to fail")
+
+
+    print("Second test, does not fail but produces wrong code"
+          "with two seperated time loops")
+
+    try:
+        bug_dimension2()
+    except:
+        print("Bug 2 not supposed to fail")
+
+    print("Third test, doesn't fail and producese correct code, however only the second stencil "
+          "has cache-blocking")
+
+    try:
+        NObug_dimension3()
+    except:
+        print("Bug 3 supposed to work")
+
+    print("Fourth test")
+
+    try:
+        bug_dimension4()
+    except:
+        print("Bug 4 not supposed to fail")
